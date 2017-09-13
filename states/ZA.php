2e33560955bb3fce8bcf9848adac8374617a890a
Monyane Ramollo <monyaneramollo@aol.com>
2017-09-13 14:30:33 +0200

    updated states-cities-and-places-for-woocommerce to version 1.1.0
     * South Africa states

---
+++ states/ZA.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * South Africa states
+ *
+ * @author  Luis Sebastian Urrutia Fuentes <luis@urrutia.me> Monyane Ramollo
+ * @version 1.0.0
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+global $states;
+
+$states ['ZA' ] = array (
+	'WC'    => 'WC Region: Western Cape',
+	'EC'    => 'EC Region: Eastern Cape',
+	'FS'    => 'FS Region: Free State',
+	'GP'    => 'GP Region: Gauteng',
+	'KZN'   => 'KZN Region: KwaZulu-Natal',
+	'LM'    => 'LP Region: Limpopo',
+	'MP'    => 'MP Region: Mpumalanga',
+	'NC'    => 'NC Region: Northern Cape',
+	'NW'    => 'NW Region: North West',
+);
+
